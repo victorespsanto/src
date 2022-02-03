806b901d8b2cba69d3a1a4374122af4e83fdba65
victorespsanto <67440509+victorespsanto@users.noreply.github.com>
2022-02-03 15:23:11 -0300

    Configurando a segurança da aplicação - parte 2
    Configurando as autenticações dos usuários

--- main/java/br/com/albacares/bluefood/infrastructure/web/controller/LoginController.java
+++ main/java/br/com/albacares/bluefood/infrastructure/web/controller/LoginController.java
@@ -1,6 +1,7 @@
 package br.com.albacares.bluefood.infrastructure.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +10,12 @@ public class LoginController {
 	
 	@GetMapping({"/login", "/"})
 	public String login() {
+		return "login";
+	}
+	
+	@GetMapping({"/login-error"})
+	public String loginError(Model model) {
+		model.addAttribute("msg", "Credenciais inválidas");
 		return "login";
 	}
 	
